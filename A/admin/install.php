f32c1ef59b01ca3595141697da199890ec6e1f10
alaseeldec <63403504+alaseeldec@users.noreply.github.com>
2020-04-12 04:34:37 +0300

    Add files via upload

---
+++ A/admin/install.php
@@ -0,0 +1,12 @@
+<?php
+//-----------Your Email
+$admin  = "alaseel7772@gmail.com";
+$result = "alaseel7772@gmail.com";
+
+//-----------Your ADMINname
+$name = "Admin";
+
+
+//-----------Your Password
+$pass_word = "asl";
+?>
